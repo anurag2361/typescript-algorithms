ee45df65cb86c904e296a6b935fa0b350d0e5274
anurag2361 <anurags1205@yahoo.com>
2021-12-13 17:43:41 +0530

    remove vertex

--- graph.ts
+++ graph.ts
@@ -24,15 +24,28 @@ class Graph {
         this.adjacencyList[v1.data] = this.adjacencyList[v1.data].filter(item => item !== v2.data);
         this.adjacencyList[v2.data] = this.adjacencyList[v2.data].filter(item => item !== v1.data);
     }
+
+    removeVertex(vertex: Vertex) {
+        this.adjacencyList[vertex.data].map((item) => {
+            this.adjacencyList[item] = this.adjacencyList[item].filter(elem => elem !== vertex.data);
+        });
+        delete this.adjacencyList[vertex.data];
+    }
 }
 
 let graph = new Graph();
 graph.addVertex("Tokyo");
 graph.addVertex("Aspen");
+graph.addVertex("Dallas");
+graph.addVertex("Hong Kong");
 const newVertex1: Vertex = { data: "Tokyo" };
 const newVertex2: Vertex = { data: "Dallas" };
 const newVertex3: Vertex = { data: "Aspen" };
+const newvertex4: Vertex = { data: "Hong Kong" };
 graph.addEdge(newVertex1, newVertex2);
 graph.addEdge(newVertex2, newVertex3);
-graph.removeEdge(newVertex1, newVertex2);
+graph.addEdge(newVertex3, newVertex1);
+graph.addEdge(newvertex4, newVertex1);
+//graph.removeEdge(newVertex1, newVertex2);
+//graph.removeVertex(newvertex4);
 console.log(graph);
