02869c91549555677a5b04dc526f11a0ca6eb573
anurag2361 <anurags1205@yahoo.com>
2021-12-14 18:15:21 +0530

    depth first iterative and breadth first search in graph

--- graph.ts
+++ graph.ts
@@ -31,6 +31,62 @@ class Graph {
         });
         delete this.adjacencyList[vertex.data];
     }
+
+    depthFirstSearchRecursive(start: Vertex) {
+        const result = [];
+        const visited: Record<string, boolean> = {};
+        const adjacencyList = this.adjacencyList;
+        (function dfs(vertex) {
+            if (!vertex.data) return null;
+            visited[vertex.data] = true;
+            result.push(vertex.data);
+            adjacencyList[vertex.data].forEach((neighbor) => {
+                if (!visited[neighbor]) {
+                    let newVert: Vertex = { data: neighbor };
+                    return dfs(newVert);
+                }
+            });
+        })(start);
+        return result;
+    }
+
+    depthFirstSearchIterative(start: Vertex) {
+        const stack = [start.data];
+        const result = [];
+        const visited: Record<string, boolean> = {};
+        let currentVertex: string = "";
+        visited[start.data] = true;
+        while (stack.length > 0) {
+            currentVertex = stack.pop()!;
+            result.push(currentVertex);
+            this.adjacencyList[currentVertex].forEach((neighbor) => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    stack.push(neighbor);
+                }
+            });
+        }
+        return result;
+    }
+
+    beadthFirst(start: Vertex) {
+        const queue = [start.data];
+        const result = [];
+        const visited: Record<string, boolean> = {};
+        let currentVertex: string = "";
+        visited[start.data] = true;
+        while (queue.length > 0) {
+            currentVertex = queue.shift()!;
+            result.push(currentVertex);
+            this.adjacencyList[currentVertex].forEach((neighbor) => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            });
+        }
+        return result;
+    }
 }
 
 let graph = new Graph();
@@ -48,4 +104,8 @@ graph.addEdge(newVertex3, newVertex1);
 graph.addEdge(newvertex4, newVertex1);
 //graph.removeEdge(newVertex1, newVertex2);
 //graph.removeVertex(newvertex4);
-console.log(graph);
+console.log(graph.depthFirstSearchRecursive(newVertex1));
+console.log(graph.depthFirstSearchIterative(newVertex1));
+console.log(graph.depthFirstSearchRecursive(newVertex1));
+console.log(graph.beadthFirst(newVertex1));
+//console.log(graph);
